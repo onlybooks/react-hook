cb6917980ef3350220703d1f42df9809a672ddbc
jeongwoo903 <www8565@naver.com>
2024-12-08 05:02:07 +0900

    refactor: UserPicker 고도화(도전과제 2.2)

--- src/components/Users/UserPicker.js
+++ src/components/Users/UserPicker.js
@@ -1,7 +1,12 @@
+import staticDB from '../../static.json'
+
 export default function UserPicker () {
+  const { users } = staticDB
+
   return (
     <select>
-      <option>Users</option>
+      <option value="" disabled selected>Users</option>
+      {users.map((user) => (<option key={user.id}>{user.name}</option>))}
     </select>
-  );
+  )
 }
